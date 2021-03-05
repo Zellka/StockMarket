bd0e842b0cb5b5dbdf025776eff2e04bd65e4a4b
Zellka <ilona.zelka@gmail.com>
2021-03-05 21:15:48 +0300

    Create FavouriteActivity

---
+++ app/src/main/java/com/example/stocktracker/ui/FavouriteActivity.kt
@@ -0,0 +1,12 @@
+package com.example.stocktracker.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.stocktracker.R
+
+class FavouriteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favourite)
+    }
+}
