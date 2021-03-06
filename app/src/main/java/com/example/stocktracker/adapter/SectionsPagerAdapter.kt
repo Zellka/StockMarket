5b4d43a8e6a1e4ed4e9a25ba4b7bb0b953b2ce1d
Zellka <ilona.zelka@gmail.com>
2021-03-06 22:09:56 +0300

    Create SectionsPagerAdapter for CardActivity

---
+++ app/src/main/java/com/example/stocktracker/adapter/SectionsPagerAdapter.kt
@@ -0,0 +1,29 @@
+package com.example.stocktracker.adapter
+
+import android.content.Context
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.example.stocktracker.R
+import com.example.stocktracker.ui.detail.PlaceholderFragment
+
+private val TAB_TITLES = arrayOf(
+    R.string.tab_text_1,
+    R.string.tab_text_2
+)
+
+class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
+    FragmentPagerAdapter(fm) {
+
+    override fun getItem(position: Int): Fragment {
+        return PlaceholderFragment.newInstance(position + 1)
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return context.resources.getString(TAB_TITLES[position])
+    }
+
+    override fun getCount(): Int {
+        return 2
+    }
+}
