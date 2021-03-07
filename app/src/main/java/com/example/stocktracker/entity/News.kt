5a57936a31239137bd4fa026cd9a527784c599a8
Zellka <ilona.zelka@gmail.com>
2021-03-07 12:46:38 +0300

    Create entity News

---
+++ app/src/main/java/com/example/stocktracker/entity/News.kt
@@ -0,0 +1,9 @@
+package com.example.stocktracker.entity
+
+data class News(
+    val title: String,
+    val image: String,
+    val publishedDate: String,
+    val text: String,
+    val url: String
+)
