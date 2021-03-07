ffef73b25887454dfe5d6500d7e3f1739fd3badd
Zellka <ilona.zelka@gmail.com>
2021-03-07 12:47:14 +0300

    Add getNews()

--- app/src/main/java/com/example/stocktracker/api/RetrofitServices.kt
+++ app/src/main/java/com/example/stocktracker/api/RetrofitServices.kt
@@ -1,10 +1,15 @@
 package com.example.stocktracker.api
 
+import com.example.stocktracker.entity.News
 import com.example.stocktracker.entity.Stock
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface RetrofitServices {
     @GET("quotes?apikey=2803cae49d539d5020e02b9de8803106")
     fun getStocks(): Call<MutableList<Stock>>
+
+    @GET("stock_news?tickers={id}&apikey=2803cae49d539d5020e02b9de8803106")
+    fun getNews(@Path("id") productId: String): Call<MutableList<News>>
 }
