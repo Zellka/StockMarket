a2bd364ae08348779133d901ddcefbee5240bb29
Zellka <ilona.zelka@gmail.com>
2021-03-19 13:55:15 +0300

    Create CardViewModel.kt

--- app/src/main/java/com/example/stocktracker/ui/detail/CardActivity.kt
+++ app/src/main/java/com/example/stocktracker/ui/detail/CardActivity.kt
@@ -1,23 +1,38 @@
 package com.example.stocktracker.ui.detail
 
-import android.content.pm.ActivityInfo
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.lifecycle.ViewModelProvider
 import androidx.viewpager.widget.ViewPager
 import com.example.stocktracker.R
 import com.example.stocktracker.adapter.SectionsPagerAdapter
+import com.example.stocktracker.entity.Stock
+import com.example.stocktracker.viewmodel.CardViewModel
 import com.google.android.material.tabs.TabLayout
 
+@Suppress("UNREACHABLE_CODE")
 class CardActivity : AppCompatActivity() {
+    private lateinit var cardViewModel: CardViewModel
+    private lateinit var stockItem: Stock
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_card)
+
+        cardViewModel = ViewModelProvider(this).get(CardViewModel::class.java)
+
         val arguments = intent.extras
         val title = arguments?.getString("TICKER_STOCK").toString()
         val subtitle = arguments?.getString("COMPANY_NAME_STOCK").toString()
+        val price = arguments?.getString("PRICE_STOCK").toString()
+        val changes = arguments?.getString("CHANGES_STOCK").toString()
+        val changesPercentage = arguments?.getString("CHANGES_PERCENTAGE_STOCK").toString()
+        stockItem = Stock(title, subtitle, price, changesPercentage, changes)
+
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
@@ -32,13 +47,35 @@ class CardActivity : AppCompatActivity() {
         tabs.setupWithViewPager(viewPager)
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            android.R.id.home -> {
-                this.finish()
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
+    @SuppressLint("UseCompatLoadingForDrawables")
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.detail_menu, menu)
+        val item = menu!!.findItem(R.id.action_favourite)
+        if (cardViewModel.isFavourite(stockItem)) {
+            item.icon = applicationContext.getDrawable(R.drawable.ic_star_select_24)
+        } else {
+            item.icon = applicationContext.getDrawable(R.drawable.ic_star_24)
         }
+        return true
+    }
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        if (id == android.R.id.home) {
+            this.finish()
+            return true
+        }
+        if (id == R.id.action_favourite) {
+            if (cardViewModel.isFavourite(stockItem)) {
+                item.icon = applicationContext.getDrawable(R.drawable.ic_star_24)
+            } else {
+                item.icon = applicationContext.getDrawable(R.drawable.ic_star_select_24)
+            }
+            cardViewModel.updateItem(stockItem)
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
---
+++ app/src/main/java/com/example/stocktracker/viewmodel/CardViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.stocktracker.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.stocktracker.data.FavouriteList
+import com.example.stocktracker.entity.Stock
+
+class CardViewModel: ViewModel() {
+    fun updateItem(stockItem: Stock) {
+        FavouriteList.updateItem(stockItem)
+    }
+
+    fun isFavourite(stockItem: Stock): Boolean{
+        return FavouriteList.isFavourite(stockItem)
+    }
+}
